d51b1d4f574c2b79733ee8edb30f7aba90ab06c8
Matheus Mota Tonini <1motaxyz@gmail.com>
2024-11-04 19:57:50 -0300

    commit

--- controllers/agendamentos.js
+++ controllers/agendamentos.js
@@ -1,48 +1,127 @@
 const db = require('../database/connection');
 
 module.exports = {
+    // async listarAgendamentos(request, response) {
+    //     try {
+    //         const { UsuarioId } = request.params;
+
+    //         const sqlUsuario = `SELECT 
+    //             a.agend_id,
+    //             a.veic_usu_id,
+    //             a.agend_data,
+    //             a.agend_horario,
+    //             a.agend_situacao,
+    //             a.agend_observ,
+    //             u.usu_id,
+    //             u.usu_nome
+    //         FROM agendamentos a
+    //         JOIN veiculo_usuario vu ON a.veic_usu_id = vu.veic_usu_id
+    //         JOIN usuarios u ON vu.usu_id = u.usu_id
+    //         WHERE u.usu_id = ?`; 
+
+    //         const [agendamentosUsuario] = await db.query(sqlUsuario, [UsuarioId]); 
+    //         const nItensUsuario = agendamentosUsuario.length;
+
+    //         const sqlTodos = `SELECT 
+    //         a.agend_id,
+    //         a.veic_usu_id,
+    //         DATE_FORMAT(a.agend_data, '%Y-%m-%d') AS agend_data_formatada,
+    //         a.agend_horario,
+    //         a.agend_situacao,
+    //         a.agend_observ,
+    //         vu.usu_id -- Adicionando o usu_id
+    //         FROM agendamentos a
+    //         JOIN veiculo_usuario vu ON a.veic_usu_id = vu.veic_usu_id`;
+
+    //         const [todosAgendamentos] = await db.query(sqlTodos);
+    //         const nItensTodos = todosAgendamentos.length;
+
+    //         const Resultado = todosAgendamentos.map((e) => ({
+    //             agend_id: e.agend_id,
+    //             usu_id: e.usu_id,
+    //             veic_usu_id: e.veic_usu_id,
+    //             veic_placa: e.veic_placa,
+    //             agend_data: e.agend_data_formatada,
+    //             agend_horario: e.agend_horario,
+    //             agend_situacao: e.agend_situacao,
+    //             agend_observ: e.agend_observ,
+    //             title: `Agendamento #${e.agend_id}`,
+    //             start: `${e.agend_data_formatada}T${e.agend_horario}`,
+    //             end: `${e.agend_data_formatada}T${e.agend_horario}`,
+    //             overlap: false,
+    //             backgroundColor: (e.usu_id == UsuarioId) ? "#FF9D00" : "#33338", 
+    //         }));
+
+    //         return response.status(200).json({
+    //             sucesso: true,
+    //             mensagem: `Lista de agendamentos do usuário ID ${UsuarioId} e todos os agendamentos.`,
+    //             dadosUsuario: agendamentosUsuario,
+    //             nItensUsuario,
+    //             dadosTodos: Resultado, 
+    //             nItensTodos
+    //         });
+    //     } catch (error) {
+    //         return response.status(500).json({
+    //             sucesso: false,
+    //             mensagem: 'Erro na requisição.',
+    //             dados: error.message
+    //         });
+    //     }
+    // },
+
     async listarAgendamentos(request, response) {
         try {
             const { UsuarioId } = request.params;
-
+    
             const sqlUsuario = `SELECT 
                 a.agend_id,
                 a.veic_usu_id,
                 a.agend_data,
                 a.agend_horario,
-                a.agend_situacao,
+                a.agend_serv_situ_id,
                 a.agend_observ,
                 u.usu_id,
-                u.usu_nome
+                u.usu_nome,
+                v.veic_placa,
+                v.veic_ano,
+                v.veic_cor
             FROM agendamentos a
             JOIN veiculo_usuario vu ON a.veic_usu_id = vu.veic_usu_id
             JOIN usuarios u ON vu.usu_id = u.usu_id
+            JOIN veiculos v ON vu.veic_id = v.veic_id
             WHERE u.usu_id = ?`; 
-
+    
             const [agendamentosUsuario] = await db.query(sqlUsuario, [UsuarioId]); 
             const nItensUsuario = agendamentosUsuario.length;
-
+    
             const sqlTodos = `SELECT 
-            a.agend_id,
-            a.veic_usu_id,
-            DATE_FORMAT(a.agend_data, '%Y-%m-%d') AS agend_data_formatada,
-            a.agend_horario,
-            a.agend_situacao,
-            a.agend_observ,
-            vu.usu_id -- Adicionando o usu_id
+                a.agend_id,
+                a.veic_usu_id,
+                DATE_FORMAT(a.agend_data, '%Y-%m-%d') AS agend_data_formatada,
+                a.agend_horario,
+                a.agend_serv_situ_id,
+                a.agend_observ,
+                vu.usu_id,
+                v.veic_placa,
+                v.veic_ano,
+                v.veic_cor
             FROM agendamentos a
-            JOIN veiculo_usuario vu ON a.veic_usu_id = vu.veic_usu_id`;
-
+            JOIN veiculo_usuario vu ON a.veic_usu_id = vu.veic_usu_id
+            JOIN veiculos v ON vu.veic_id = v.veic_id`;
+    
             const [todosAgendamentos] = await db.query(sqlTodos);
             const nItensTodos = todosAgendamentos.length;
-
+    
             const Resultado = todosAgendamentos.map((e) => ({
                 agend_id: e.agend_id,
                 usu_id: e.usu_id,
                 veic_usu_id: e.veic_usu_id,
+                veic_placa: e.veic_placa,
+                veic_ano: e.veic_ano,
+                veic_cor: e.veic_cor,
                 agend_data: e.agend_data_formatada,
                 agend_horario: e.agend_horario,
-                agend_situacao: e.agend_situacao,
+                agend_serv_situ_id: e.agend_serv_situ_id,
                 agend_observ: e.agend_observ,
                 title: `Agendamento #${e.agend_id}`,
                 start: `${e.agend_data_formatada}T${e.agend_horario}`,
@@ -50,7 +129,7 @@ module.exports = {
                 overlap: false,
                 backgroundColor: (e.usu_id == UsuarioId) ? "#FF9D00" : "#33338", 
             }));
-
+    
             return response.status(200).json({
                 sucesso: true,
                 mensagem: `Lista de agendamentos do usuário ID ${UsuarioId} e todos os agendamentos.`,
@@ -66,7 +145,7 @@ module.exports = {
                 dados: error.message
             });
         }
-    },
+    },    
 
     async listarTodosAgendamentos(request, response) {
         try {
@@ -237,13 +316,13 @@ module.exports = {
     async editarSituacaoAgendamento(request, response) {
         try {
             const { agend_id } = request.params;
-            const { agend_situacao } = request.body;
+            const { agend_serv_situ_id } = request.body;
 
             const sql = `UPDATE agendamentos SET 
-                agend_situacao = ? 
+                agend_serv_situ_id = ? 
                 WHERE agend_id = ?`;
 
-            const values = [agend_situacao, agend_id];
+            const values = [agend_serv_situ_id, agend_id];
 
             const [atualizaDados] = await db.query(sql, values);
 
